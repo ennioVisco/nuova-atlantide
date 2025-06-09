7b284ba155874b1ad73fd118cab3cc409097ac9a
Ennio Visconti <ennio.visconti@gmail.com>
2025-06-09 20:49:01 +0200

    repo init
    ci: fixed wrong branch name
    
    fix: build fix
    
    fix: removed wrong lock file
    
    ci: forcing corepack
    
    ci: fixed urls

--- astro.config.ts
+++ astro.config.ts
@@ -5,11 +5,9 @@ import UnoCSS from 'unocss/astro';
 export default defineConfig({
   // used to generate images
   site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+    process.env.LOCAL_DEV === 'true'
+      ? 'https://localhost:3000/'
+      : 'https://enniovisco.github.io/nuova-atlantide/',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
